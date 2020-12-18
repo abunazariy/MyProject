cc215d33edfa1259cc3cdc64fc5157d7a6d8a056
Abdulla Nazarov <abunazariy@gmail.com>
2020-12-18 09:17:56 -0500

    first commit

---
+++ src/test/java/com/bnymellon/step_definitions/loginStepDefinitions.java
@@ -0,0 +1,20 @@
+package com.bnymellon.step_definitions;
+
+import io.cucumber.java.en.*;
+
+public class loginStepDefinitions {
+
+    @Given("user is on login page")
+    public void user_is_on_login_page() {
+
+    }
+
+    @When("user enters valid credentials")
+    public void user_enters_valid_credentials() {
+
+    }
+    @Then("user should see dashboard page")
+    public void user_should_see_dashboard_page() {
+
+    }
+}
